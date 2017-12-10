ecb6de5decef00d207abd4b0dae313c2bc8fb641
zhexuany <yangzhexuan@pingcap.com>
2017-12-10 12:15:35 -0600

    update binary files

--- stop_cluster.sh
+++ stop_cluster.sh
@@ -1,8 +1,6 @@
 pkill -9 pd-server
 pkill -9 tidb-server
 pkill -9 tikv-server
-if [ -f ./log/*.log.* ]; then
-  rm -f ./log/*.log.*
-  rm -rf tikv
-  rm -rf pd
-fi
+rm -f ./log/*.log.*
+rm -rf tikv
+rm -rf pd
