42a0404e72b605484a206a036516384cbb036dc5
zhexuany <yangzhexuan@pingcap.com>
2018-03-06 10:38:43 +0800

    add config and update scripts

--- run_cluster.sh
+++ run_cluster.sh
@@ -7,10 +7,11 @@ pdaddr="127.0.0.1:2379"
 
 # start pd-server
 mkdir -p log
-./bin/pd-server --data-dir=pd --log-file=./log/pd.log >/dev/null &
+mkdir -p data
+./bin/pd-server --data-dir=data/pd --log-file=./log/pd.log >/dev/null&
 
 sleep 10s
 # start tikv-server
-./bin/tikv-server --pd=$pdaddr --store=tikv -C ./etc/config-template.toml --log-file ./log/tikv.log&
+./bin/tikv-server --pd=$pdaddr --store=data/tikv --config=conf/tikv.toml --log-file ./log/tikv.log&
 # start tidb-server
-./bin/tidb-server --store=tikv --path=$pdaddr --log-file=./log/tidb.log&
+./bin/tidb-server --store=tikv --config=conf/tidb.toml --path=$pdaddr --log-file=./log/tidb.log&
--- stop_cluster.sh
+++ stop_cluster.sh
@@ -2,5 +2,3 @@ pkill -9 pd-server
 pkill -9 tidb-server
 pkill -9 tikv-server
 rm -f ./log/*.log.*
-rm -rf tikv
-rm -rf pd
